281885a7a758754ac37bdc01c1b0cacd767a0615
erickpalomino1923 <erickpalomino0723@gmail.com>
2024-05-29 01:12:41 -0500

    Update PhysiodeskBackendApplication.java
    test

--- src/main/java/physiodesk/physiodesk_backend/PhysiodeskBackendApplication.java
+++ src/main/java/physiodesk/physiodesk_backend/PhysiodeskBackendApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PhysiodeskBackendApplication {
 
-	publicd static void main(String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(PhysiodeskBackendApplication.class, args);
 	}
+
 
 }
