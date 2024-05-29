e3614560892f748c9dc05408761cb0540d614715
erickpalomino1923 <erickpalomino0723@gmail.com>
2024-05-29 00:19:33 -0500

    Initial commit

---
+++ src/test/java/physiodesk/physiodesk_backend/PhysiodeskBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package physiodesk.physiodesk_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhysiodeskBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
