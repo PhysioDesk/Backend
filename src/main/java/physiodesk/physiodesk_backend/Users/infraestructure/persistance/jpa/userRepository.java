8ce3d47a41d988de3818d7d9385a467386da4dc3
erickpalomino1923 <erickpalomino0723@gmail.com>
2024-06-06 11:20:45 -0500

    add: User Validation

--- src/main/java/physiodesk/physiodesk_backend/Users/infraestructure/persistance/jpa/userRepository.java
+++ src/main/java/physiodesk/physiodesk_backend/Users/infraestructure/persistance/jpa/userRepository.java
@@ -1,4 +1,16 @@
 package physiodesk.physiodesk_backend.Users.infraestructure.persistance.jpa;
 
-public interface userRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import physiodesk.physiodesk_backend.Users.domain.model.aggregates.user;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface userRepository extends JpaRepository<user, Long> {
+    @Query("SELECT u FROM user u WHERE u.id = :id")
+    Optional<user> GetUserById(@Param("id") Long id);
+    @Query("SELECT u FROM user u")
+    List<user> GetAllUsers();
 }
