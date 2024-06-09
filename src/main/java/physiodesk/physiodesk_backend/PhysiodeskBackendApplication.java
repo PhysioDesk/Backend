7d0d380a1a9662f5f8a2c72551ae751a8461cd5f
erickpalomino1923 <erickpalomino0723@gmail.com>
2024-06-09 01:09:16 -0500

    Merge WebService

--- src/main/java/physiodesk/physiodesk_backend/PhysiodeskBackendApplication.java
+++ src/main/java/physiodesk/physiodesk_backend/PhysiodeskBackendApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PhysiodeskBackendApplication {
 
-	publicd static void main(String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(PhysiodeskBackendApplication.class, args);
 	}
 
